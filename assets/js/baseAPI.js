f85061561d652b31aa47b624a3228314843e1c10
qiucaiping <1243666322@qq.com>
2020-09-03 14:56:24 +0800

    index over

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,18 @@
 $.ajaxPrefilter(function(options) {
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    //设置访问权限
+
+    options.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            console.log(11);
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
+
 })
